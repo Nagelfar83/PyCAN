cec6152fbac759a6097dcd71acb0da842cdb8053
Jim Nilsson <jim.gd.nilsson@gmail.com>
2016-11-12 14:54:26 +0100

    Added copywrite docummenation and licens information.

---
+++ Library/__init__.py
@@ -0,0 +1,23 @@
+'''
+Terms and conditions
+--------------------
+
+Copywrite 2009 Jim Nilsson.
+PyCAN is distributed under the terms of the GNU Lesser General Public License.
+
+    This file is part of the PyCAN Libary.
+
+    PyCAN is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyCAN is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU General Public License and
+    GNU Lesser General Public License along with PyCAN.  
+    If not, see <http://www.gnu.org/licenses/>.
+'''
